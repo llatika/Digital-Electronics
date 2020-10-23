9b7637e976ad945da942a8ab5812e8920b733396
patrycjapiotrowska <patrycja.piotrowska@edu.fh-joanneum.at>
2020-10-23 15:06:15 +0200

    Simulate Combinational Logic with Vivado

---
+++ Lab3 Simulating Combinational Logic/Task1/Task1.srcs/sources_1/imports/uni_comp.sv
@@ -0,0 +1,25 @@
+module uni_comp(
+	input A,
+	inputB,
+	input output_en,  //when inactive, outputs are LOW
+	output logic A_lt_B, // A smaller than B
+	output logic A_eq_B, // A equal to B
+	output logic A_bt_B  // A bigger than B
+	);
+	
+	always_comb
+	begin
+	if(output_en)
+	begin
+		A_lt_B = (~A) & B;
+		A_bt_B =A & (~B);
+		A_eq_B = ~( A_lt_B | A_bt_B);
+	end
+	else
+	begin
+	A_lt_B = 1'b0;
+	A_bt_B = 1'b0;
+	A_eq_B = 1'b0;
+	end
+	end
+	endmodule
